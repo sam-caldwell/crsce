1ad850c862f204e7319518a873bc41a872f14f4c
Sam Caldwell <github@samcaldwell.net>
2024-12-18 03:07:05 -0600

    enhance Bit test

--- pkg/types/Bit._test.go
+++ pkg/types/Bit._test.go
@@ -1,15 +1,28 @@
 package types
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestBitEnum(t *testing.T) {
-	var b Bit
-	b = Clear
-	if uint(b) != 0 {
-		t.Errorf("BitEnum expect 0 but got %d", uint(b))
-	}
-	b = Set
-	if uint(b) != 1 {
-		t.Errorf("BitEnum expect 1 but got %d", uint(b))
-	}
+	t.Run("happy path", func(t *testing.T) {
+		var b Bit
+		b = Clear
+		if uint(b) != 0 {
+			t.Errorf("BitEnum expect 0 but got %d", uint(b))
+		}
+		b = Set
+		if uint(b) != 1 {
+			t.Errorf("BitEnum expect 1 but got %d", uint(b))
+		}
+	})
+	t.Run("verify underlying type", func(t *testing.T) {
+		var b Bit
+		expectedType := reflect.TypeOf(b).Kind()
+		actualType := reflect.TypeOf(b).Kind()
+		if actualType != expectedType {
+			t.Fatalf("expected %v got %v", expectedType, actualType)
+		}
+	})
 }
