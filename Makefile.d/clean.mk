e96509fa5bb2a68b3178aba102a000a7a45622ef
Sam Caldwell <github@samcaldwell.net>
2026-01-16 23:49:10 -0600

    polishing clean.mk while on a call

--- Makefile.d/clean.mk
+++ Makefile.d/clean.mk
@@ -1,10 +1,14 @@
 # Makefile.d/clean.mk
 # (c) 2026 Sam Caldwell. See LICENSE.txt for details
+#
+# Look at me using ❌ and ✅ like a youngster.  Take that ya hipsters!
+# I may be old but I can adapt new ways AND enjoy music from an era when
+# people actually played an instrument.
 
 .PHONY: all build clean configure help lint ready ready/fix test
 clean:
 	@echo "--- Cleaning build directory ---"
 	@[ -d "$(CURDIR)/build" ] && rm -rf $(CURDIR)/build && echo "    ✅ Removed build directory."
-	@mkdir -p $(CURDIR)/build
+	@mkdir -p $(CURDIR)/build || {echo "❌ failed to create build directory" && exit 1}
 	@echo "    ✅ Created empty build directory."
 	@echo "--- Clean complete ---"
