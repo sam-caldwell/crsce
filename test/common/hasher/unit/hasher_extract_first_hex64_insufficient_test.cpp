98f84d95fb5f2694483698d79bb0c65c75920533
Sam Caldwell <github@samcaldwell.net>
2026-01-20 08:40:58 -0600

    make src into constexpr

--- test/common/hasher/unit/hasher_extract_first_hex64_insufficient_test.cpp
+++ test/common/hasher/unit/hasher_extract_first_hex64_insufficient_test.cpp
@@ -9,7 +9,7 @@
 using crsce::common::hasher::extract_first_hex64;
 
 TEST(HasherUtils, ExtractFirstHex64Insufficient) { // NOLINT
-    const std::string src = "1234567890abcdef"; // too short
+    constexpr std::string src = "1234567890abcdef"; // too short
     std::string out;
     EXPECT_FALSE(extract_first_hex64(src, out));
 }
