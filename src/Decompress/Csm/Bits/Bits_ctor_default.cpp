2c322574428df4fcfb187002bfdea173df537cc7
Sam Caldwell <github@samcaldwell.net>
2026-02-14 23:19:38 -0600

    linter fix

--- src/Decompress/Csm/Bits/Bits_ctor_default.cpp
+++ src/Decompress/Csm/Bits/Bits_ctor_default.cpp
@@ -1,9 +1,16 @@
 /**
  * @file Bits_ctor_default.cpp
+ * @author Sam Caldwell
+ * @brief Implementation
+ * @copyright (c) 2026 Sam Caldwell. See LICENSE.txt for details
  */
 #include "decompress/Csm/detail/Bits.h"
 
 namespace crsce::decompress {
+    /**
+     * @name Bits
+     * @brief default constructor
+     */
     Bits::Bits() = default;
 }
 
