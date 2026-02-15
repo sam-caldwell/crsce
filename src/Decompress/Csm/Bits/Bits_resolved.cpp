819ecb50b806427b4f65907907f540d2169e60d7
Sam Caldwell <github@samcaldwell.net>
2026-02-14 23:32:37 -0600

    Linter fix

--- src/Decompress/Csm/Bits/Bits_resolved.cpp
+++ src/Decompress/Csm/Bits/Bits_resolved.cpp
@@ -1,9 +1,18 @@
 /**
  * @file Bits_resolved.cpp
+ * @author Sam Caldwell
+ * @brief Implementation
+ * @copyright (c) 2026 Sam Caldwell. See LICENSE.txt for details
  */
 #include "decompress/Csm/detail/Bits.h"
 
 namespace crsce::decompress {
-    bool Bits::resolved() const noexcept { return locked(); }
+    /**
+     * @name resolved
+     * @brief return the cell's resolved state
+     * @return bool
+     */
+    bool Bits::resolved() const noexcept {
+        return (b_ & kResolved) != 0U;
+    }
 }
-
