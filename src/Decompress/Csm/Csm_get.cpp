e72919388b759f794d0fe1d30977f15c8a97b4e0
Sam Caldwell <github@samcaldwell.net>
2026-02-14 22:34:29 -0600

    clean up get_rc

--- src/Decompress/Csm/Csm_get.cpp
+++ src/Decompress/Csm/Csm_get.cpp
@@ -1,12 +1,21 @@
 /**
  * @file Csm_get.cpp
- * @brief Implementation
+ * @author Sam Caldwell
+ * @brief Implement get_rc() to return cell value at CSM cell (r,c)
+ * @copyright (c) 2026 Sam Caldwell.  See LICENSE.txt for details
  */
-#include "decompress/Csm/detail/Csm.h"
+#include "decompress/Csm/Csm.h"
 #include "common/exceptions/CsmIndexOutOfBounds.h"
 #include <cstddef>
 
 namespace crsce::decompress {
+    /**
+     * @name get_rc
+     * @brief Using (r,c) space, return the CSM cell value.
+     * @param r row
+     * @param c column
+     * @return boolean cell value (set/clear)
+     */
     bool Csm::get_rc(const std::size_t r, const std::size_t c) const {
         if (!in_bounds(r, c)) { throw CsmIndexOutOfBounds(r, c, kS); }
         return cells_.at(r).at(c).data();
