c629926e1020d662a5856353f471c4202ace0507
Sam Caldwell <github@samcaldwell.net>
2026-02-14 22:28:30 -0600

    clean up src/Decompress/Csm/Csm_data.cpp

--- src/Decompress/Csm/Csm_data.cpp
+++ src/Decompress/Csm/Csm_data.cpp
@@ -1,14 +1,27 @@
 /**
  * @file Csm_data.cpp
- * @brief Implementation
+ * @author Sam Caldwell
+ * @brief Csm method to return belief data.
+ * @copyright (c) 2026 Sam Caldwell. See License.txt for details.
  */
-#include "decompress/Csm/detail/Csm.h"
+#include "decompress/Csm/Csm.h"
 #include "common/exceptions/CsmIndexOutOfBounds.h"
 #include <cstddef>
 
 namespace crsce::decompress {
+    /**
+     * @name get_data
+     * @brief return the given cell's belief data
+     * @param r row
+     * @param c column
+     * @return belief data (double)
+     */
     double Csm::get_data(const std::size_t r, const std::size_t c) const {
-        if (!in_bounds(r, c)) { throw CsmIndexOutOfBounds(r, c, kS); }
+
+        if (!in_bounds(r, c)) {
+            throw CsmIndexOutOfBounds(r, c, kS);
+        }
+
         return data_.at(r).at(c);
     }
 } // namespace crsce::decompress
