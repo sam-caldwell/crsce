4cf191b4dc2317a02be560a55959eaef107dadd3
Sam Caldwell <github@samcaldwell.net>
2026-02-14 23:21:47 -0600

    linter fix

--- src/Decompress/Csm/Bits/Bits_ctor_flags.cpp
+++ src/Decompress/Csm/Bits/Bits_ctor_flags.cpp
@@ -1,11 +1,21 @@
 /**
  * @file Bits_ctor_flags.cpp
+ * @author Sam Caldwell
+ * @brief Implementation
+ * @copyright (c) 2026 Sam Caldwell.  See License.txt for details
  */
 #include "decompress/Csm/detail/Bits.h"
 #include <cstdint>
 
 namespace crsce::decompress {
-    Bits::Bits(const bool data, const bool lock, const bool mu)
-        : b_(static_cast<std::uint8_t>((data ? kData : 0U) | (mu ? kMu : 0U) | (lock ? kLock : 0U))) {}
+    /**
+     * @name Bits
+     * @brief Class constructor
+     * @param data cell value (boolean) indicating 0|1
+     * @param resolved cell state (resolved means it is solved for decompression and readonly)
+     * @param mu mutex lock for async operations.
+     */
+    Bits::Bits(const bool data, const bool resolved, const bool mu)
+        : b_(static_cast<std::uint8_t>((data ? kData : 0U) | (mu ? kMu : 0U) | (resolved ? kResolved : 0U))) {
+    }
 }
-
