9fe5a99a291d48165c69279f248c79e10dc51144
Sam Caldwell <github@samcaldwell.net>
2026-02-14 23:25:13 -0600

    linter fix

--- src/Decompress/Csm/Bits/Bits_data.cpp
+++ src/Decompress/Csm/Bits/Bits_data.cpp
@@ -1,9 +1,19 @@
 /**
  * @file Bits_data.cpp
+ * @author Sam Caldwell
+ * @brief Implementation
+ * @copyright (c) 2026 Sam Caldwell. See LICENSE.txt for details
  */
 #include "decompress/Csm/detail/Bits.h"
 
 namespace crsce::decompress {
-    bool Bits::data() const noexcept { return (b_ & kData) != 0U; }
+    /**
+     * @name data
+     * @brief return the cell value (data bit)
+     * @return boolean (set/clear) value
+     */
+    bool Bits::data() const noexcept {
+        return (b_ & kData) != 0U;
+    }
 }
 
