0c3b7598a7920f34057a7fc36b217ae06c155268
Sam Caldwell <github@samcaldwell.net>
2026-02-14 22:28:11 -0600

    clean up csm consrtuctor

--- src/Decompress/Csm/Csm_ctor.cpp
+++ src/Decompress/Csm/Csm_ctor.cpp
@@ -1,12 +1,18 @@
 /**
  * @file Csm_ctor.cpp
- * @brief Implementation
+ * @brief Csm Class constructor
+ * @author Sam Caldwell
+ * @copyright (c) 2026 Sam Caldwell. See License.txt for details.
  */
-#include "decompress/Csm/detail/Csm.h"
+#include "decompress/Csm/Csm.h"
 #include <cstddef>
 #include <atomic>
 
 namespace crsce::decompress {
+    /**
+     * @name Csm
+     * @brief class constructor
+     */
     Csm::Csm() {
         // Zero storage
         for (std::size_t r = 0; r < kS; ++r) {
@@ -14,9 +20,15 @@ namespace crsce::decompress {
                 cells_.at(r).at(c).clear();
                 data_.at(r).at(c) = 0.0;
             }
+
             row_versions_.at(r).store(0ULL, std::memory_order_relaxed);
-            lsm_c_.at(r) = 0; vsm_c_.at(r) = 0; dsm_c_.at(r) = 0; xsm_c_.at(r) = 0;
+
+            lsm_c_.at(r) = 0;
+            vsm_c_.at(r) = 0;
+            dsm_c_.at(r) = 0;
+            xsm_c_.at(r) = 0;
         }
         build_dx_tables();
     }
+
 } // namespace crsce::decompress
