6142c321c8a56e72f19ff2a292aafd437cefdf5c
Sam Caldwell <github@samcaldwell.net>
2026-02-14 23:11:13 -0600

    ensure get_data is updated

--- src/Decompress/Csm/Csm_data.cpp
+++ src/Decompress/Csm/Csm_data.cpp
@@ -18,9 +18,7 @@ namespace crsce::decompress {
      */
     double Csm::get_data(const std::size_t r, const std::size_t c) const {
 
-        if (!in_bounds(r, c)) {
-            throw CsmIndexOutOfBounds(r, c, kS);
-        }
+        bounds_check(r, c);
 
         return data_.at(r).at(c);
     }
