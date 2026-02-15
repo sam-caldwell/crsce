7e371b5d904aee3d5636b4baf7aa8a426d163e17
Sam Caldwell <github@samcaldwell.net>
2026-02-14 22:32:22 -0600

    clean up src/Decompress/Csm/Csm_flush.cpp

---
+++ src/Decompress/Csm/Csm_flush.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file Csm_flush.cpp
+ * @author Sam Caldwell
+ * @brief defines a flush method
+ * @copyright (c) 2026 Sam Caldwell.  See License.txt for details.
+ */
+#include "decompress/Csm/Csm.h"
+
+namespace crsce::decompress {
+    /**
+     * @name flush
+     * @brief a reserved (nop) flush method for future use
+     */
+    void Csm::flush() noexcept {}
+}
+
