d5753cf958457d01bc223511be7e76b1634a002c
Sam Caldwell <github@samcaldwell.net>
2026-02-16 14:30:43 -0600

    linter fix

--- src/common/O11y/O11y.cpp
+++ src/common/O11y/O11y.cpp
@@ -6,8 +6,32 @@
  */
 #include "common/O11y/O11y.h"
 
-// Implementation and its internal includes
-#include "detail/O11y_impl.inc"
+#include <algorithm>
+#include <chrono>
+#include <coroutine>
+#include <cstdint>
+#include <mutex>
+#include <functional>
+#include <optional>
+#include <queue>
+#include <ios>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <stop_token>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <cstdlib>
+#include <atomic>
+
+#include "common/O11y/detail/escape_json.h"
+#include "common/O11y/detail/events_path.h"
+#include "common/O11y/detail/now_ms.h"
+#include "common/O11y/detail/write_line_sync.h"
+#include "common/O11y/detail/flush_enabled.h"
+#include "common/O11y/detail/write_line_to_path_sync.h"
+
 namespace crsce::o11y {
 /**
  * @name instance
@@ -15,7 +39,11 @@ namespace crsce::o11y {
  * @return O11y& Reference to the singleton instance.
  */
 O11y &O11y::instance() {
-    static O11y inst; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+    static O11y inst; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables,misc-use-anonymous-namespace)
     return inst;
 }
+
+// Pull in the implementation inside the target namespace so dependent symbols resolve.
+#include "detail/O11y_impl.inc"
+
 } // namespace crsce::o11y
